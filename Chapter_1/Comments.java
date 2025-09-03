f8df81cd8d25ef0daa8f4e64db6439e4541823de
KristeneCavan <kristenecahan888@gmail.com>
2025-09-04 00:01:15 +0800

    Create Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,17 @@
+/**
+ * This is a documentation comment.
+ * It explains the 3 types of comments in Java.
+ */
+public class Comments {
+
+    public static void main(String[] args) {
+        // This is a single-line comment. It explains one line of a code.
+        System.out.println("Hello, Comments!");
+
+        /* 
+         This is a multi-line comment.
+         You can write comments here on many lines.
+        */
+        System.out.println("This program shows 3 types of comments.");
+    }
+}
