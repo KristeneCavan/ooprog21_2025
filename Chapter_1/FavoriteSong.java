e45f65512ac4b97ca012fd998ef995b5f7307a6b
KristeneCavan <kristenecahan888@gmail.com>
2025-09-03 23:53:12 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("I still remember the day we met");
+        System.out.println("The feeling has never gone away");
+        System.out.println("Through the years, it’s still the same");
+        System.out.println("I’m still into you every single day");
+    }
+}
