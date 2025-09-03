b710015556339160d1c104f08d09c2eed08a5c8b
KristeneCavan <kristenecahan888@gmail.com>
2025-09-03 23:58:05 +0800

    Create FirstDialog.java

---
+++ Chapter_1/FirstDialog.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class FirstJavaDialog {
+    public static void main(String[] args) {
+        // Display a message dialog with a custom title
+        JOptionPane.showMessageDialog(null, "First Java Dialog", "Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
