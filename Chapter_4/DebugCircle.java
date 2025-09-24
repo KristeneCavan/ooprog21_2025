ab5b91efc974402588c9135c451f99e5163e6471
KristeneCavan <kristenecahan888@gmail.com>
2025-09-24 08:22:26 +0800

    Update DebugCircle.java

--- Chapter_4/DebugCircle.java
+++ Chapter_4/DebugCircle.java
@@ -1,28 +1,22 @@
-public class DebugCircle{
-   private int radius;
-   private int diameter;
-   private final double PI = 3.14159;
-   private double area;
+public class DebugCircle {
+    private int radius;
+    private int diameter;
+    private final double PI = 3.14159;
 
-   public DebugCircle(int r)
-   {
-      radius = r;
-      diameter = 2 * r;
-      area = PI * r * r;
-   }
+    public DebugCircle(int r) {
+        radius = r;
+        diameter = 2 * r;
+    }
 
-   public int getRadius()
-   {
-      return radius;
-   }
+    public int getRadius() {
+        return radius;
+    }
 
-   public int getDiameter()
-   {
-      return diameter;
-   }
+    public int getDiameter() {
+        return diameter;
+    }
 
-   public double getArea()
-   {
-      return area;
-   }
+    public double getArea() {
+        return PI * radius * radius;
+    }
 }
