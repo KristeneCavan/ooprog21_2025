36c38abec56d7d52bee4622fb0f7619691460c3c
KristeneCavan <kristenecahan888@gmail.com>
2025-09-24 08:45:13 +0800

    Update DebugPen.java

--- Chapter_4/DebugPen.java
+++ Chapter_4/DebugPen.java
@@ -1,26 +1,30 @@
-public class DebugPen{
+public class DebugPen {
     private String color;
     private String point;
 
-    public DebugPen()
-    {
+    public DebugPen() {
         color = "black";
         point = "fine";
     }
 
-    public DebugPen(String color, String point)
-    {
+    public DebugPen(String color, String point) {
         this.color = color;
         this.point = point;
     }
 
-    public String getColor()
-    {
+    public String getColor() {
         return color;
     }
 
-    public String getPoint()
-    {
+    public String getPoint() {
         return point;
+    }
+
+    public static void main(String[] args) {
+        DebugPen defaultPen = new DebugPen();
+        DebugPen customPen = new DebugPen("blue", "medium");
+
+        System.out.println("Default Pen: color = " + defaultPen.getColor() + ", point = " + defaultPen.getPoint());
+        System.out.println("Custom Pen: color = " + customPen.getColor() + ", point = " + customPen.getPoint());
     }
 }
